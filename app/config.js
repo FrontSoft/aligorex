8ba2b144ad2b8e158931c347737a2987d1bd45fb
muftiev.rr <muftiev.rr@yandex.ua>
2014-07-03 00:14:29 +0400

    contacts page view + VM

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 define(function(){
     return {
         "app": {
-            "mainPage": "Company",
-            "firstPage": "Company"
+            "mainPage": "Contacts",
+            "firstPage": "Contacts"
         }
     };
 });
