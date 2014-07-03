0a39b8ad45e12a7f803156a2d0ce7f8a04858e88
muftiev.rr <muftiev.rr@yandex.ua>
2014-07-03 23:58:21 +0400

    sideMenu + Services View partly + Services VM partly

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 define(function(){
     return {
         "app": {
-            "mainPage": "Contacts",
-            "firstPage": "Contacts"
+            "mainPage": "Company",
+            "firstPage": "Company"
         }
     };
 });
--- app/pages/Company.View.js
+++ app/pages/Company.View.js
@@ -2,8 +2,56 @@ define(['kover'], function(KOVER){
 
     var Page = KOVER.NewPage('Company'),
     Regions = Page.layout({
+        SwipedMenu: KOVER.Ui('nav', {sideMenu: {}, class: "main-nav"}),
         Header: KOVER.Ui('header'),
-        Body: KOVER.Ui('main')
+        Body: KOVER.Ui('main'),        
+    });
+
+    Regions.SwipedMenu({
+        TopMenu: KOVER.Ui('ul', {
+            items: [
+                {Home: KOVER.Ui('a', {
+                    text: 'Главная',
+                    Name: KOVER.Ui('img', {}),
+                    // click: function(){KOVER.GoTo('Home');}
+                } )},
+                {Company: KOVER.Ui('a', {
+                    text: 'О компании',
+                    Name: KOVER.Ui('img', {}),
+                    click: function(){KOVER.GoTo('Company');}
+                } )},
+                {Works: KOVER.Ui('a', {
+                    text: 'Работы',
+                    Name: KOVER.Ui('img', {}),
+                    // click: function(){KOVER.GoTo('Works');}
+                } )},
+                {Services: KOVER.Ui('a', {
+                    text: 'Услуги компании',
+                    Name: KOVER.Ui('img', {}),
+                    click: function(){KOVER.GoTo('Services');}
+                } )},
+                {Contacts: KOVER.Ui('a', {
+                    text: 'Контакты',
+                    Name: KOVER.Ui('img', {}),
+                    click: function(){KOVER.GoTo('Contacts');}
+                } )},
+                {Team: KOVER.Ui('a', {
+                    text: 'Команда',
+                    Name: KOVER.Ui('img', {}),
+                    // click: function(){KOVER.GoTo('Team');}
+                } )},
+                {Map: KOVER.Ui('a', {
+                    text: 'Мы на карте',
+                    Name: KOVER.Ui('img', {}),
+                    // click: function(){KOVER.GoTo('Map');}
+                } )},
+                {News: KOVER.Ui('a', {
+                    text: 'Новости',
+                    Name: KOVER.Ui('img', {}),
+                    // click: function(){KOVER.GoTo('News');}
+                } )},
+            ]
+        })        
     });
 
     Regions.Header({
